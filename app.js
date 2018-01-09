d77eff032cd8cad48667336124af3f49555295e8
Swarnambika <j.swarnambika@gmail.com>
2018-01-09 22:49:11 +0530

    Clean up code. Add error messages when required.

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
 const http = require('http');
 
+const
+    ERR_PARSE_FAIL = 'Could not decode request: JSON parsing failed',
+    ERR_NO_HANDLER = 'Could not handle this request. USAGE: POST application/json to /filter end-point.',
+    ERR_REQ_READ = 'Error reading JSON data.';
+
+const
+    HTTP_CODE_BAD_REQUEST = 400,
+    HTTP_CODE_OK = 200;
+
 
 const server = http.createServer((req, res) => {
 
     const { method, url, body, headers } = req;
-    console.log(method, url, body, headers['content-type']);
+    console.log(':==>', method, url, headers['content-type']);
 
     if (method == 'POST' && url == '/filter'
         && headers['content-type'] == 'application/json') {
 
         let body = [];
         req.on('error', err => {
-            res.write(err);
-
-            res.statusCode = 400;
-            res.end();
+            console.log('X==>', err);
+            respondOut(res, JSON.stringify(errorObject(ERR_REQ_READ)), HTTP_CODE_BAD_REQUEST);
         }).on('data', data => {
             body.push(data);
         }).on('end', () => {
             body = Buffer.concat(body).toString();
 
-            const payloads = JSON.parse(body)["payload"];
+            let payloads = [];
+            try {
+                payloads = JSON.parse(body)["payload"];
+            }
+            catch (err) {
+                console.log('X==>', err);
+                respondOut(res, JSON.stringify(errorObject(ERR_PARSE_FAIL)), HTTP_CODE_BAD_REQUEST);
+                return;
+            }
             const response = payloads
                 .filter(pl => pl.type == 'htv' && pl.workflow == 'completed')
                 .reduce((prev, pl) => {
-                    let concataddress = Object.keys(pl.address).map(key => pl.address[key]).join(' ');
+                    let concataddress = Object
+                        .keys(pl.address)
+                        .map(key => pl.address[key])
+                        .join(' ');
 
                     prev.push({ 'concataddress': concataddress, 'type': 'htv', 'completed': 'completed' });
                     return prev;
                 }, []);
 
-            res.write(JSON.stringify({ 'response': response }));
-            res.statusCode = 200;
-            res.end();
+            respondOut(res, JSON.stringify({ 'response': response }), HTTP_CODE_OK);
         });
     }
     else {
-        res.statusCode = 400;
-        res.end();
+        respondOut(res, JSON.stringify(errorObject(ERR_NO_HANDLER)), HTTP_CODE_BAD_REQUEST);
     }
 
 });
+
+function respondOut(res, msg, statusCode) {
+    res.statusCode = statusCode;
+    res.write(msg);
+    res.end()
+}
+
+function errorObject(err) {
+    return { 'error': err };
+}
 
 
 const port = process.env.PORT || 3000;
